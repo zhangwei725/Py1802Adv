0bac90bbfd121f62adf0239caf9a33bea8ae87cc
zhangwei <26558563@qq.com>
2018-06-04 11:52:27 +0800

    添加session

--- apps/session01/urls.py
+++ apps/session01/urls.py
@@ -5,4 +5,9 @@ from apps.session01 import views
 urlpatterns = [
     url('cookie/', views.test_cookie),
     url('test02/', views.test02),
+    url('1/', views.session01),
+    url('2/', views.session02),
+    url('3/', views.del_session),
+    url('4/', views.set_exp),
+
 ]
